1109bffe102908a39b188ce35401a939f8628cfb
Tim Glaser <tim.glaser@hiberly.com>
2020-03-10 16:07:20 -0700

    Update version in settings

--- settings.py
+++ settings.py
@@ -17,6 +17,8 @@ import sentry_sdk
 
 from sentry_sdk.integrations.django import DjangoIntegration
 
+VERSION = '1.0.7'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
