c6445729dfbf91027922d1b21f0c43de0863acb3
Tim Glaser <tim.glaser@hiberly.com>
2020-03-25 16:57:34 +0000

    Bump version 1.0.9

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ import sentry_sdk
 
 from sentry_sdk.integrations.django import DjangoIntegration
 
-VERSION = '1.0.8.3'
+VERSION = '1.0.9'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
