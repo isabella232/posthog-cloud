f1760280fbc538655c29965de2b0fff7b46c9f0a
Kunal <kunal@posthog.com>
2021-10-07 09:25:28 -0700

    update signup tests (#141)
    * update signup tests
    
    * update black version
    
    Co-authored-by: kunal <kunal@Kunals-iMac.hsd1.ca.comcast.net>
    Co-authored-by: eric <eeoneric@gmail.com>

--- multi_tenancy/tests/test_signup.py
+++ multi_tenancy/tests/test_signup.py
@@ -10,7 +10,9 @@ class TestTeamSignup(CloudAPIBaseTest):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
-        User.objects.create(email="firstuser@posthog.com",)  # to ensure consistency in tests
+        User.objects.create(
+            email="firstuser@posthog.com",
+        )  # to ensure consistency in tests
 
     @patch("messaging.tasks.process_organization_signup_messaging.delay")
     @patch("posthoganalytics.identify")
@@ -58,8 +60,11 @@ class TestTeamSignup(CloudAPIBaseTest):
         self.assertEqual(organization.name, "Hedgehogs United, LLC")
         self.assertEqual(team.name, "Default Project")
 
+        mock_capture.assert_called_once()
+        self.assertEqual(user.distinct_id, mock_capture.call_args.args[0])
+        self.assertEqual("user signed up", mock_capture.call_args.args[1])
         # Assert that the sign up event & identify calls were sent to PostHog analytics
-        analytics_props = {
+        key_analytics_props = {
             "is_first_user": False,
             "is_organization_first_user": True,
             "new_onboarding_enabled": False,
@@ -67,12 +72,13 @@ class TestTeamSignup(CloudAPIBaseTest):
             "signup_social_provider": "",
             "realm": "cloud",
         }
-        mock_capture.assert_called_once_with(
-            user.distinct_id, "user signed up", properties=analytics_props,
-        )
+        event_props = mock_capture.call_args.kwargs["properties"]
+        for prop, val in key_analytics_props.items():
+            self.assertEqual(event_props[prop], val)
 
         mock_identify.assert_called_once_with(
-            user.distinct_id, analytics_props,
+            user.distinct_id,
+            key_analytics_props,
         )
 
         # Assert that the user is logged in
@@ -84,12 +90,16 @@ class TestTeamSignup(CloudAPIBaseTest):
         self.assertTrue(user.check_password("notsecure"))
 
         # Check that the process_organization_signup_messaging task was fired
-        mock_messaging.assert_called_once_with(user_id=user.id, organization_id=str(organization.id))
+        mock_messaging.assert_called_once_with(
+            user_id=user.id, organization_id=str(organization.id)
+        )
 
     @patch("messaging.tasks.process_organization_signup_messaging.delay")
     @patch("posthoganalytics.identify")
     @patch("posthoganalytics.capture")
-    def test_api_sign_up_existing_email(self, mock_capture, mock_identify, mock_messaging):
+    def test_api_sign_up_existing_email(
+        self, mock_capture, mock_identify, mock_messaging
+    ):
         response = self.client.post(
             "/api/signup/",
             {
@@ -103,7 +113,8 @@ class TestTeamSignup(CloudAPIBaseTest):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         response.data.pop("detail")  # Message might be changed in main repo
         self.assertEqual(
-            response.data, {"type": "validation_error", "code": "unique", "attr": "email"},
+            response.data,
+            {"type": "validation_error", "code": "unique", "attr": "email"},
         )
 
     @patch("posthoganalytics.capture")
@@ -116,7 +127,12 @@ class TestTeamSignup(CloudAPIBaseTest):
 
         response = self.client.post(
             "/api/signup/",
-            {"first_name": "John", "email": "hedgehog5@posthog.com", "password": "notsecure", "email_opt_in": False,},
+            {
+                "first_name": "John",
+                "email": "hedgehog5@posthog.com",
+                "password": "notsecure",
+                "email_opt_in": False,
+            },
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -127,18 +143,22 @@ class TestTeamSignup(CloudAPIBaseTest):
         self.assertEqual(user.email, "hedgehog5@posthog.com")
 
         # Assert that the sign up event & identify calls were sent to PostHog analytics
-        mock_capture.assert_called_once_with(
-            user.distinct_id,
-            "user signed up",
-            properties={
-                "is_first_user": False,
-                "is_organization_first_user": True,
-                "new_onboarding_enabled": False,
-                "signup_backend_processor": "OrganizationSignupSerializer",
-                "signup_social_provider": "",
-                "realm": "cloud",
-            },
-        )
+        mock_capture.assert_called_once()
+
+        self.assertEqual(user.distinct_id, mock_capture.call_args.args[0])
+        self.assertEqual("user signed up", mock_capture.call_args.args[1])
+        # Assert that the sign up event & identify calls were sent to PostHog analytics
+        key_analytics_props = {
+            "is_first_user": False,
+            "is_organization_first_user": True,
+            "new_onboarding_enabled": False,
+            "signup_backend_processor": "OrganizationSignupSerializer",
+            "signup_social_provider": "",
+            "realm": "cloud",
+        }
+        event_props = mock_capture.call_args.kwargs["properties"]
+        for prop, val in key_analytics_props.items():
+            self.assertEqual(event_props[prop], val)
 
         # Assert that the user is logged in
         response = self.client.get("/api/users/@me/")
@@ -146,13 +166,18 @@ class TestTeamSignup(CloudAPIBaseTest):
         self.assertEqual(response.json()["email"], "hedgehog5@posthog.com")
 
         # Check that the process_organization_signup_messaging task was fired
-        mock_messaging.assert_called_once_with(user_id=user.id, organization_id=str(organization.id))
+        mock_messaging.assert_called_once_with(
+            user_id=user.id, organization_id=str(organization.id)
+        )
 
     @patch("posthoganalytics.capture")
     @patch("messaging.tasks.process_organization_signup_messaging.delay")
     def test_user_can_sign_up_with_a_custom_plan(self, mock_messaging, mock_capture):
         plan = Plan.objects.create(
-            key="startup", default_should_setup_billing=True, price_id="price_12345678", name="Test Plan",
+            key="startup",
+            default_should_setup_billing=True,
+            price_id="price_12345678",
+            name="Test Plan",
         )
 
         response = self.client.post(
@@ -180,22 +205,26 @@ class TestTeamSignup(CloudAPIBaseTest):
 
         # Check that the process_organization_signup_messaging task was fired
         mock_messaging.assert_called_once_with(
-            user_id=user.id, organization_id=str(organization.id),
+            user_id=user.id,
+            organization_id=str(organization.id),
         )
 
         # Check that we send the sign up event to PostHog analytics
-        mock_capture.assert_called_once_with(
-            user.distinct_id,
-            "user signed up",
-            properties={
-                "is_first_user": False,
-                "is_organization_first_user": True,
-                "new_onboarding_enabled": False,
-                "signup_backend_processor": "OrganizationSignupSerializer",
-                "signup_social_provider": "",
-                "realm": "cloud",
-            },
-        )
+        mock_capture.assert_called_once()
+        self.assertEqual(user.distinct_id, mock_capture.call_args.args[0])
+        self.assertEqual("user signed up", mock_capture.call_args.args[1])
+        # Assert that the sign up event & identify calls were sent to PostHog analytics
+        key_analytics_props = {
+            "is_first_user": False,
+            "is_organization_first_user": True,
+            "new_onboarding_enabled": False,
+            "signup_backend_processor": "OrganizationSignupSerializer",
+            "signup_social_provider": "",
+            "realm": "cloud",
+        }
+        event_props = mock_capture.call_args.kwargs["properties"]
+        for prop, val in key_analytics_props.items():
+            self.assertEqual(event_props[prop], val)
 
     @patch("posthoganalytics.capture")
     @patch("messaging.tasks.process_organization_signup_messaging.delay")
@@ -203,7 +232,12 @@ class TestTeamSignup(CloudAPIBaseTest):
 
         response = self.client.post(
             "/api/signup/",
-            {"first_name": "Jane", "email": "hedgehog6@posthog.com", "password": "notsecure", "plan": "NOTVALID",},
+            {
+                "first_name": "Jane",
+                "email": "hedgehog6@posthog.com",
+                "password": "notsecure",
+                "plan": "NOTVALID",
+            },
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -217,35 +251,46 @@ class TestTeamSignup(CloudAPIBaseTest):
         )  # OrganizationBilling is not created yet
 
         # Check that we send the sign up event to PostHog analytics
-        mock_capture.assert_called_once_with(
-            user.distinct_id,
-            "user signed up",
-            properties={
-                "is_first_user": False,
-                "is_organization_first_user": True,
-                "new_onboarding_enabled": False,
-                "signup_backend_processor": "OrganizationSignupSerializer",
-                "signup_social_provider": "",
-                "realm": "cloud",
-            },
-        )
+        mock_capture.assert_called_once()
+        self.assertEqual(user.distinct_id, mock_capture.call_args.args[0])
+        self.assertEqual("user signed up", mock_capture.call_args.args[1])
+        # Assert that the sign up event & identify calls were sent to PostHog analytics
+        key_analytics_props = {
+            "is_first_user": False,
+            "is_organization_first_user": True,
+            "new_onboarding_enabled": False,
+            "signup_backend_processor": "OrganizationSignupSerializer",
+            "signup_social_provider": "",
+            "realm": "cloud",
+        }
+        event_props = mock_capture.call_args.kwargs["properties"]
+        for prop, val in key_analytics_props.items():
+            self.assertEqual(event_props[prop], val)
 
         # Check that the process_organization_signup_messaging task was fired
         mock_messaging.assert_called_once_with(
-            user_id=user.pk, organization_id=str(organization.id),
+            user_id=user.pk,
+            organization_id=str(organization.id),
         )
 
     @patch("messaging.tasks.process_organization_signup_messaging.delay")
     @patch("posthoganalytics.capture")
     def test_sign_up_multiple_teams_multi_tenancy(
-        self, mock_capture, mock_messaging,
+        self,
+        mock_capture,
+        mock_messaging,
     ):
 
         # Create a user first to make sure additional users can be created
         User.objects.create(email="i_was_first@posthog.com")
 
         response = self.client.post(
-            "/api/signup/", {"first_name": "John", "email": "multi@posthog.com", "password": "eruceston",},
+            "/api/signup/",
+            {
+                "first_name": "John",
+                "email": "multi@posthog.com",
+                "password": "eruceston",
+            },
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -267,18 +312,21 @@ class TestTeamSignup(CloudAPIBaseTest):
         self.assertEqual(user.email, "multi@posthog.com")
 
         # Assert that the sign up event & identify calls were sent to PostHog analytics
-        mock_capture.assert_called_once_with(
-            user.distinct_id,
-            "user signed up",
-            properties={
-                "is_first_user": False,
-                "is_organization_first_user": True,
-                "new_onboarding_enabled": False,
-                "signup_backend_processor": "OrganizationSignupSerializer",
-                "signup_social_provider": "",
-                "realm": "cloud",
-            },
-        )
+        mock_capture.assert_called_once()
+        self.assertEqual(user.distinct_id, mock_capture.call_args.args[0])
+        self.assertEqual("user signed up", mock_capture.call_args.args[1])
+        # Assert that the sign up event & identify calls were sent to PostHog analytics
+        key_analytics_props = {
+            "is_first_user": False,
+            "is_organization_first_user": True,
+            "new_onboarding_enabled": False,
+            "signup_backend_processor": "OrganizationSignupSerializer",
+            "signup_social_provider": "",
+            "realm": "cloud",
+        }
+        event_props = mock_capture.call_args.kwargs["properties"]
+        for prop, val in key_analytics_props.items():
+            self.assertEqual(event_props[prop], val)
 
         # Assert that the user is logged in
         response = self.client.get("/api/users/@me/")
@@ -290,5 +338,6 @@ class TestTeamSignup(CloudAPIBaseTest):
 
         # Check that the process_organization_signup_messaging task was fired
         mock_messaging.assert_called_once_with(
-            user_id=user.pk, organization_id=str(user.organization.id),
+            user_id=user.pk,
+            organization_id=str(user.organization.id),
         )
