6f19c4634b7277382c5e631f41fd9cf5a22a2a22
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-03-16 10:53:12 +0100

    feat: extend API router (#176)

---
+++ multi_tenancy/router.py
@@ -0,0 +1,6 @@
+def extend_api_router(router, organizations_router, projects_router, **kwargs) -> None:
+    """
+    Extends nested routers from main repo. Cannot use urls.py due to a circular import issue.
+    This method is imported from urls.py on the main repo.
+    """
+    pass
